540eceead244d3313429b45e5cf07df0daf297cc
nrr <nrr@warsong.me>
2022-06-09 12:15:28 +0300

    added products classes

---
+++ src/main/java/ru/netology/products/Book.java
@@ -0,0 +1,23 @@
+package ru.netology.products;
+
+public class Book extends Product {
+    private String author;
+
+
+    public Book() {
+        super();
+    }
+
+    public Book(int id, String name, int cost, String author) {
+        super(id, name, cost);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/ru/netology/products/Smartphone.java
@@ -0,0 +1,22 @@
+package ru.netology.products;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int cost, String manufacturer) {
+        super(id, name, cost);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
