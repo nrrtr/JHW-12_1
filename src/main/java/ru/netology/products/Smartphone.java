a1db933d523d0919f2e212ba837d97e4bbdc33fb
nrr <nrr@warsong.me>
2022-06-09 13:43:54 +0300

    added matches method in Product; overrided matches method in extended classes; fixed searchBy method in Manager

--- src/main/java/ru/netology/products/Smartphone.java
+++ src/main/java/ru/netology/products/Smartphone.java
@@ -19,4 +19,13 @@ public class Smartphone extends Product {
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
+
+    @Override
+    public boolean matches(String search) {
+        if (!super.matches(search)) {
+            return this.getManufacturer().contains(search);
+        } else {
+            return true;
+        }
+    }
 }
